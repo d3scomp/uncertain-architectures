4d96d66afbe2265ae6690c72d2e6cd8c28915d65
Ilias Gerostathopoulos <iliasger@gmail.com>
2016-01-05 17:39:14 +0200

    setting path to logging.properties in Simulations.py

--- uncertain-architectures/analysis/Simulations.py
+++ uncertain-architectures/analysis/Simulations.py
@@ -9,11 +9,14 @@ from subprocess import *
 root = os.path.dirname(os.path.realpath(__file__))
 classpath = os.path.join(root, '..' , 'dist' ,'*' + os.pathsep + root, '..' + os.pathsep + '.')
 
+logPath = "src\logging.properties"
+
 print('Spawning simulation processes...')
-# invoke 10 iterations of with the same configuration
+# invoke 10 iterations with the same configuration
 for i in range(1,11):
     cmd = ['java', 
         '-classpath', classpath,
+        '-Djava.util.logging.config.file=%s' % (logPath.replace('\\', '/')),
         'cz.cuni.mff.d3s.jdeeco.ua.demo.Run',
         'logs/log_' + str(i)]
     simulation = Popen(cmd)

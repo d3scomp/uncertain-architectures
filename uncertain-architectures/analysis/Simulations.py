d67648d909dce8cbcca6dc960f2b355403974cc1
Ilias Gerostathopoulos <iliasger@gmail.com>
2016-01-08 11:54:36 +0200

    Simulation are now invoked sequentially from Python

--- uncertain-architectures/analysis/Simulations.py
+++ uncertain-architectures/analysis/Simulations.py
@@ -6,20 +6,25 @@ Created on Dec 31, 2015
 import os
 from subprocess import *
 
-root = os.path.dirname(os.path.realpath(__file__))
-classpath = os.path.join(root, '..' , 'dist' ,'*' + os.pathsep + root, '..' + os.pathsep + '.')
+def simulate():
+    root = os.path.dirname(os.path.realpath(__file__))
+    classpath = os.path.join(root, '..' , 'dist' ,'*' + os.pathsep + root, '..' + os.pathsep + '.')
+    
+    logPath = "src\logging.properties"
+    
+    print('Spawning simulation processes...')
+    # invoke 10 iterations with the same configuration
+    for i in range(1,11):
+        cmd = ['java', 
+            '-classpath', classpath,
+            '-Djava.util.logging.config.file=%s' % (logPath.replace('\\', '/')),
+            'cz.cuni.mff.d3s.jdeeco.ua.demo.Run',
+            'logs/log_' + str(i)]
+        simulation = Popen(cmd)
+        simulation.wait()
+        
+if __name__ == '__main__': 
 
-logPath = "src\logging.properties"
-
-print('Spawning simulation processes...')
-# invoke 10 iterations with the same configuration
-for i in range(1,11):
-    cmd = ['java', 
-        '-classpath', classpath,
-        '-Djava.util.logging.config.file=%s' % (logPath.replace('\\', '/')),
-        'cz.cuni.mff.d3s.jdeeco.ua.demo.Run',
-        'logs/log_' + str(i)]
-    simulation = Popen(cmd)
-print('Spawning done.')
-
+    simulate()
+    print("Simulations finished.")
 

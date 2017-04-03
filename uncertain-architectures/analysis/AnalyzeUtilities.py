8271425a31a6e670a44e273f378b33f3fd79872b
Dominik Skoda <skoda@d3s.mff.cuni.cz>
2017-04-03 19:13:31 +0200

    Script for utility analysis.

---
+++ uncertain-architectures/analysis/AnalyzeUtilities.py
@@ -0,0 +1,96 @@
+'''
+Created on Dec 8, 2015
+
+This script extracts and computes the dirtiness duration times from logs
+produced by simulations.
+
+@author: Ilias
+@author: Dominik Skoda
+'''
+
+import os
+import sys
+import shutil
+import re
+import array
+import numpy as np
+import matplotlib.pyplot as plt
+import matplotlib.text as mpltext
+
+from Configuration import *
+from Scenarios import *
+from traitlets.config.application import catch_config_error
+
+
+
+class StringLabel(object):
+    def __init__(self, text, color):
+        self.my_text = text
+        self.my_color = color
+
+
+class StringLabelHandler(object):
+    def legend_artist(self, legend, orig_handle, fontsize, handlebox):
+        x0, y0 = handlebox.xdescent, handlebox.ydescent
+        width, height = handlebox.width, handlebox.height
+        patch = mpltext.Text(x = 0, y = 0, text = orig_handle.my_text,
+                             color = orig_handle.my_color,
+                             verticalalignment = u'baseline',
+                             horizontalalignment = u'left',
+                             multialignment = None, fontproperties = None,
+                             rotation = 0, linespacing = None, rotation_mode = None)
+        handlebox.add_artist(patch)
+        return patch
+    
+
+def analyzeLog(signature):
+    
+    logsDir = os.path.join(LOGS_DIR, signature, UMS_LOGS)
+    utilities = {}
+    
+    for dirname, dirnames, filenames in os.walk(logsDir):
+        # print path to all filenames.
+        for filename in filenames:
+            print("Processing: {}".format(os.path.join(dirname, filename)))
+            transition = os.path.basename(dirname)
+            if not utilities.__contains__(transition):
+                utilities[transition] = []
+            lineCnt = 0
+            with open(os.path.join(dirname, filename)) as f:
+                for line in f:
+                    utilities[transition].append(int(line))
+                    lineCnt = lineCnt + 1
+            print("Found {} records.".format(lineCnt))
+            print("Done.")
+
+    print("Ploting...")
+    labels = []
+    values = []
+    signatures = []
+    i = 1
+    for k in utilities.keys():
+        signatures.append(k)
+        labels.append(StringLabel(str(i), "black"))
+        i = i + 1
+        values.append(utilities[k])
+        
+    plt.boxplot(values)
+    plt.legend(labels, signatures, handler_map = {StringLabel:StringLabelHandler()})
+    plt.savefig("{}.png".format(os.path.join(LOGS_DIR, signature, "utilities")))
+    print("Done.")
+
+
+if __name__ == '__main__':
+    try:
+        if len(sys.argv) < 2:
+            raise ArgError("The script expects one number as a parameter.")
+        
+    
+        print("Started.")
+        signature = getScenarioSignature(int(sys.argv[1]))
+        analyzeLog(signature)
+        print("Finished.")
+    except ArgError as e:
+        print(e.__str__())
+        print("usage: python AnalyzeUtilities.py <scenario>")
+    
